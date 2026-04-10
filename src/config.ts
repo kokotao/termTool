cf3448762e4b9ef625bb9b00838fe1401a9b5928
albert_luo <480199976@qq.com>
2026-04-10 10:00:41 +0800

    fix: regenerate square icons and release v0.1.4

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export const appConfig = {
-  version: '0.1.3',
+  version: '0.1.4',
   author: 'albert_luo',
   authorEmail: '480199976@qq.com',
   website: 'https://github.com/albertluo', // Example link
